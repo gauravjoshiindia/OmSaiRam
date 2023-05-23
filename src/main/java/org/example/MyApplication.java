15934286b70dd5663ae7dc376610fa3cd11f0f0c
gauravjoshiindia@gmail.com <gauravjoshiindia@gmail.com>
2023-05-23 00:07:31 -0400

    Added Swagger Methods Supported

--- src/main/java/org/example/MyApplication.java
+++ src/main/java/org/example/MyApplication.java
@@ -4,13 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
 public class MyApplication {
 
-    @RequestMapping("/")
+    @RequestMapping(value="/", method = RequestMethod.GET)
     String home(){
         return "OmSaiRam!!!!!!";
     }
